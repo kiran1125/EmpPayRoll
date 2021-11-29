bfb04c9d97a8cab4a6aa87b3f78933341402119e
Kiran <makamkiran95@gmail.com>
2021-11-29 22:59:16 +0530

    Added : Reading the data from the file

--- src/main/java/com/bridgelabz/App.java
+++ src/main/java/com/bridgelabz/App.java
@@ -3,6 +3,7 @@ package com.bridgelabz;
 public class App {
     public static void main(String[] args) {
         EmployeePayRollService employeePayRollService = new EmployeePayRollService();
+        employeePayRollService.readDataFromFile();
 
     }
 }
